b2def8e65f162804d757eaa62bc6e9f90f3e1ca5
daniqss <danielqueijo14@gmail.com>
2025-10-23 16:30:40 +0200

    feat(#7): finish add with carry instruction
    closes #7

--- src/core/instructions/arithmetic.rs
+++ src/core/instructions/arithmetic.rs
@@ -4,6 +4,7 @@ use crate::core::{
 };
 
 /// add the value in register plus the carry flag to A register
+/// probably should I use wrapping_add here?
 pub fn adc_a_r8(r8: u8, f: u8, a: &mut u8) -> InstructionEffect {
     let mut result = *a + r8;
     result += if (f & CARRY_FLAG_MASK) != 0 { 1 } else { 0 };
@@ -11,6 +12,30 @@ pub fn adc_a_r8(r8: u8, f: u8, a: &mut u8) -> InstructionEffect {
     InstructionEffect::new(
         1,
         1,
+        Some(check_zero(result) | check_carry(result, *a) | check_half_carry(result, *a)),
+    )
+}
+
+/// add the value pointed by HL pair of registers plus de carry to A register
+pub fn adc_a_hl(value: u8, f: u8, a: &mut u8) -> InstructionEffect {
+    let mut result = *a + value;
+    result += if (f & CARRY_FLAG_MASK) != 0 { 1 } else { 0 };
+
+    InstructionEffect::new(
+        2,
+        1,
+        Some(check_zero(result) | check_carry(result, *a) | check_half_carry(result, *a)),
+    )
+}
+
+/// add immediate value n8 plus the carry flag to A register
+pub fn adc_a_n8(n8: u8, f: u8, a: &mut u8) -> InstructionEffect {
+    let mut result = *a + n8;
+    result += if (f & CARRY_FLAG_MASK) != 0 { 1 } else { 0 };
+
+    InstructionEffect::new(
+        2,
+        2,
         Some(check_zero(result) | check_carry(result, *a) | check_half_carry(result, *a)),
     )
 }
