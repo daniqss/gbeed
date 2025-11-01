7c3458e6610e418975608f6dc68373a9d7b476d1
daniqss <danielqueijo14@gmail.com>
2025-11-01 17:28:07 +0100

    feat(#8): new implementation for add with carry
    closes #8

--- src/core/cpu/instructions/adc.rs
+++ src/core/cpu/instructions/adc.rs
@@ -1,20 +1,26 @@
+use std::fmt::Write;
+
 use super::InstructionTarget as IT;
 use crate::core::cpu::{
     flags::{CARRY_FLAG_MASK, check_carry, check_half_carry, check_zero},
-    instructions::{InstructionEffect, InstructionError, InstructionResult},
+    instructions::{Instruction, InstructionEffect, InstructionError, InstructionResult},
 };
 
 /// Add with carry instruction
-///
+/// Adds the value of the specified target plus the carry flag to register A
 pub struct ADC<'a> {
+    a: &'a mut u8,
+    f: &'a mut u8,
     addend: IT<'a>,
 }
 
 impl<'a> ADC<'a> {
-    pub fn new(addend: IT<'a>) -> Self { ADC { addend } }
+    pub fn new(a: &'a mut u8, f: &'a mut u8, addend: IT<'a>) -> Self { ADC { a, f, addend } }
+}
 
-    pub fn exec(addend: IT, f: u8, a: &mut u8) -> InstructionResult {
-        let (addend, cycles, len) = match addend {
+impl<'a> Instruction<'a> for ADC<'a> {
+    fn exec(&mut self) -> InstructionResult {
+        let (addend, cycles, len) = match self.addend {
             IT::Immediate(n8) => (n8, 2, 2),
             IT::RegisterB(b) => (b, 1, 1),
             IT::RegisterC(c) => (c, 1, 1),
@@ -28,30 +34,34 @@ impl<'a> ADC<'a> {
 
         // perform the addition
         // wrapping it prevent overflow panics in debug mode
-        let mut result = a.wrapping_add(addend);
-        result = result.wrapping_add(if (f & CARRY_FLAG_MASK) != 0 { 1 } else { 0 });
-        let flags = check_zero(result) | check_carry(result, *a) | check_half_carry(result, *a);
+        let mut result = self.a.wrapping_add(addend);
+        result = result.wrapping_add(if (*self.f & CARRY_FLAG_MASK) != 0 {
+            1
+        } else {
+            0
+        });
+        let flags =
+            check_zero(result) | check_carry(result, *self.a) | check_half_carry(result, *self.a);
 
         Ok(InstructionEffect::new(len, cycles, Some(flags)))
     }
-}
 
-impl std::fmt::Display for ADC<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn disassembly(&self, w: &mut dyn Write) -> Result<(), InstructionError> {
         write!(
-            f,
-            "ADC A,{}",
+            w,
+            "adc a,{}",
             match self.addend {
                 IT::Immediate(n8) => format!("{}", n8),
-                IT::RegisterB(_) => "B".to_string(),
-                IT::RegisterC(_) => "C".to_string(),
-                IT::RegisterD(_) => "D".to_string(),
-                IT::RegisterE(_) => "E".to_string(),
-                IT::RegisterH(_) => "H".to_string(),
-                IT::RegisterL(_) => "L".to_string(),
-                IT::PointedByHL(_) => "[HL]".to_string(),
-                _ => unreachable!("ADC instruction doesn't support target {:?}", self.addend),
+                IT::RegisterB(_) => "b".to_string(),
+                IT::RegisterC(_) => "c".to_string(),
+                IT::RegisterD(_) => "d".to_string(),
+                IT::RegisterE(_) => "e".to_string(),
+                IT::RegisterH(_) => "h".to_string(),
+                IT::RegisterL(_) => "l".to_string(),
+                IT::PointedByHL(_) => "[hl]".to_string(),
+                _ => return Err(InstructionError::MalformedInstruction),
             }
         )
+        .map_err(|_| InstructionError::MalformedInstruction)
     }
 }
