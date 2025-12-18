ea2871b529c521148dfcfd94c0edb9482b497678
daniqss <danielqueijo14@gmail.com>
2025-12-18 13:43:55 +0100

    feat(#24): add raylib dependencies, and configure different supported display environments
    close #24

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     fenix,
     ...
@@ -19,36 +20,96 @@
           overlays = [fenix.overlays.default];
         }));
   in {
-    devShells = eachSystem (system: pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.cargo
-          pkgs.cargo-expand
-          pkgs.rust-analyzer
-          pkgs.rustc
-          pkgs.clippy
-          fenix.packages.${system}.latest.rustfmt
+    devShells = eachSystem (system: pkgs: let
+      commonPackages = with pkgs; [
+        just
 
-          pkgs.SDL2
-          pkgs.SDL2_mixer
-          pkgs.SDL2_image
-          pkgs.SDL2_ttf
-          pkgs.SDL2_gfx
-        ];
+        cargo
+        cargo-expand
+        rust-analyzer
+        rustc
+        clippy
+        fenix.packages.${system}.latest.rustfmt
 
+        cmake
+        clang
+      ];
+
+      x11Packages = with pkgs; [
+        xorg.libX11
+        xorg.libXrandr
+        xorg.libXinerama
+        xorg.libXcursor
+        xorg.libXi
+      ];
+
+      waylandPackages = with pkgs; [
+        wayland
+        glfw
+        libxkbcommon
+      ];
+
+      drmPackages = with pkgs; [
+        pkg-config
+        libdrm
+        libgbm
+        mesa
+        libGL
+        libglvnd
+      ];
+    in {
+      # shell for x11 environments
+      x11 = pkgs.mkShell {
+        buildInputs = commonPackages ++ x11Packages;
+
+        DISPLAY_FEATURES = "";
         RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 
-        # used to set SDL_VIDEODRIVER depending on the graphical session
-        shellHook = ''
-          if [ "$XDG_SESSION_TYPE" = "wayland" ] || [ -n "$WAYLAND_DISPLAY" ]; then
-            export SDL_VIDEODRIVER=wayland
-          elif [ "$XDG_SESSION_TYPE" = "x11" ] || [ -n "$DISPLAY" ]; then
-            export SDL_VIDEODRIVER=x11
-          else
-            export SDL_VIDEODRIVER=kmsdrm
-          fi
-        '';
+        LD_LIBRARY_PATH = with pkgs;
+          lib.makeLibraryPath [
+            libGL
+            xorg.libX11
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+            alsa-lib
+          ];
       };
+
+      # shell for wayland environments
+      wayland = pkgs.mkShell {
+        buildInputs = commonPackages ++ waylandPackages;
+
+        DISPLAY_FEATURES = "raylib/wayland raylib/USE_EXTERNAL_GLFW";
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+
+        LD_LIBRARY_PATH = with pkgs;
+          lib.makeLibraryPath [
+            alsa-lib
+          ];
+      };
+
+      # shell for drm environments
+      drm = pkgs.mkShell {
+        buildInputs = commonPackages ++ drmPackages;
+
+        DISPLAY_FEATURES = "raylib/drm raylib/opengl_es_20";
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+
+        NIX_CFLAGS_COMPILE = "-I${pkgs.libdrm.dev}/include/libdrm";
+
+        LD_LIBRARY_PATH = with pkgs;
+          lib.makeLibraryPath [
+            alsa-lib
+          ];
+      };
+
+      # defaulting to x11 because wayland will use it over xwayland anyway
+      default = self.devShells.${system}.x11;
     });
   };
 }
