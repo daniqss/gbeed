4398218d890944b4ec9266fb9f8fb9e4ed83ff50
daniqss <danielqueijo14@gmail.com>
2025-12-19 12:08:52 +0100

    fix(#24): fix dev shell in x11
    close #24

--- flake.nix
+++ flake.nix
@@ -33,80 +33,85 @@
 
         cmake
         clang
-      ];
-
-      x11Packages = with pkgs; [
-        xorg.libX11
-        xorg.libXrandr
-        xorg.libXinerama
-        xorg.libXcursor
-        xorg.libXi
-      ];
-
-      waylandPackages = with pkgs; [
-        wayland
         glfw
-        libxkbcommon
-      ];
-
-      drmPackages = with pkgs; [
-        pkg-config
-        libdrm
-        libgbm
-        mesa
-        libGL
-        libglvnd
       ];
     in {
       # shell for x11 environments
-      x11 = pkgs.mkShell {
-        buildInputs = commonPackages ++ x11Packages;
+      x11 = let
+        x11Packages = with pkgs; [
+          libGL
+          xorg.libX11
+          xorg.libXrandr
+          xorg.libXinerama
+          xorg.libXcursor
+          xorg.libXi
+          alsa-lib
+        ];
+        x11Features = "";
+      in
+        pkgs.mkShell {
+          buildInputs = commonPackages ++ x11Packages;
 
-        DISPLAY_FEATURES = "";
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          DISPLAY_FEATURES = x11Features;
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 
-        LD_LIBRARY_PATH = with pkgs;
-          lib.makeLibraryPath [
-            libGL
-            xorg.libX11
-            xorg.libXrandr
-            xorg.libXinerama
-            xorg.libXcursor
-            xorg.libXi
-            alsa-lib
-          ];
-      };
+          LD_LIBRARY_PATH = with pkgs;
+            lib.makeLibraryPath x11Packages;
+        };
 
       # shell for wayland environments
-      wayland = pkgs.mkShell {
-        buildInputs = commonPackages ++ waylandPackages;
+      wayland = let
+        waylandPackages = with pkgs; [
+          wayland
+          libxkbcommon
+          alsa-lib
+        ];
+        waylandFeatures = [
+          "raylib/wayland"
+          "raylib/USE_EXTERNAL_GLFW"
+        ];
+      in
+        pkgs.mkShell {
+          buildInputs = commonPackages ++ waylandPackages;
 
-        DISPLAY_FEATURES = "raylib/wayland raylib/USE_EXTERNAL_GLFW";
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          DISPLAY_FEATURES = pkgs.lib.concatStringsSep " " waylandFeatures;
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 
-        LD_LIBRARY_PATH = with pkgs;
-          lib.makeLibraryPath [
-            alsa-lib
-          ];
-      };
+          LD_LIBRARY_PATH = with pkgs;
+            lib.makeLibraryPath waylandPackages;
+        };
 
       # shell for drm environments
-      drm = pkgs.mkShell {
-        buildInputs = commonPackages ++ drmPackages;
+      drm = let
+        drmPackages = with pkgs; [
+          pkg-config
+          libdrm
+          libgbm
+          mesa
+          libGL
+          libglvnd
+        ];
+        drmFeatures = [
+          "raylib/drm"
+          "raylib/opengl_es_20"
+        ];
+      in
+        pkgs.mkShell {
+          buildInputs = commonPackages ++ drmPackages;
 
-        DISPLAY_FEATURES = "raylib/drm raylib/opengl_es_20";
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          DISPLAY_FEATURES = pkgs.lib.concatStringsSep " " drmFeatures;
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 
-        NIX_CFLAGS_COMPILE = "-I${pkgs.libdrm.dev}/include/libdrm";
+          NIX_CFLAGS_COMPILE = "-I${pkgs.libdrm.dev}/include/libdrm";
 
-        LD_LIBRARY_PATH = with pkgs;
-          lib.makeLibraryPath [
-            alsa-lib
-          ];
-      };
+          LD_LIBRARY_PATH = with pkgs;
+            lib.makeLibraryPath [
+              alsa-lib
+            ];
+        };
 
       # defaulting to x11 because wayland will use it over xwayland anyway
       default = self.devShells.${system}.x11;
